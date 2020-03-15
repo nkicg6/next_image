a9f5da36d79dba4f53cc3b0dbe01f0a4c2061f13
Nicholas George <nicholas.george32@gmail.com>
2020-03-14 20:35:50 -0600

    added simple tests and cognitect clj test runner

--- src/next_image/core.clj
+++ src/next_image/core.clj
@@ -9,23 +9,12 @@
   "holds all state for app"
   (atom {:todo-files nil
          :done-files nil
-         :files-remaining nil
-         :files-done nil}))
+         :todo-files-count nil
+         :done-files-count nil}))
 
 (defn update-todo-done-field!
   "updates todo adn done lists. Also updates metadata like file lengths"
   [state-atom kw thing]
   (swap! state-atom assoc kw thing)
-  (swap! state-atom assoc :files-remaining (count (:done-files @state-atom)))
-  (swap! state-atom assoc :files-done (count (:todo-files @state-atom))))
-
-;;;; repl play and development
-
-(defn add-test-files [v]
-   v)
-
-(update-todo-done-field! next-image-state :todo-files (add-test-files ["f1" "f2" "f3"]))
-(update-todo-done-field! next-image-state :done-files (add-test-files ["done1" "done2"]))
-
-@next-image-state
-
+  (swap! state-atom assoc :todo-files-count (count (:todo-files @state-atom)))
+  (swap! state-atom assoc :done-files-count (count (:done-files @state-atom))))
--- test/next_image/core_test.clj
+++ test/next_image/core_test.clj
@@ -1,1 +1,15 @@
 ;; test code
+(ns next-image.core-test
+  (:require [clojure.test :refer :all]
+            [next-image.core :as core]))
+
+(deftest update-atom
+  (let [todos ["f1" "f2" "f3"]
+        dones ["d1" "d2"]]
+    (core/update-todo-done-field! core/next-image-state :todo-files todos)
+    (is (= todos (:todo-files @core/next-image-state)))
+    (is (= (count todos) (:todo-files-count @core/next-image-state)))
+    (core/update-todo-done-field! core/next-image-state :done-files dones)
+    (is (= dones (:done-files @core/next-image-state)))
+    (is (= (count dones) (:done-files-count @core/next-image-state)))))
+
