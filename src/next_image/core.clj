9bcca1cd30105b0d6f634fd8f00c2c1c59272eea
Nicholas George <nicholas.george32@gmail.com>
2020-03-15 22:04:34 -0600

    started gui example

--- src/next_image/core.clj
+++ src/next_image/core.clj
@@ -1,8 +1,9 @@
 (ns next-image.core
-  (:require [seesaw.core :as sc]))
+  (:require [seesaw.core :as seesaw]))
 
-;; left off on line 182 of https://gist.github.com/daveray/1441520 where it starts to get useful 
-(sc/native!)
+;; left off on line 182 of https://gist.github.com/daveray/1441520 where it starts to get useful
+;; see listbox example https://github.com/daveray/seesaw/blob/master/test/seesaw/test/examples/scroll.clj
+(seesaw/native!)
 ;; list files
 
 (def next-image-state
@@ -13,8 +14,36 @@
          :done-files-count nil}))
 
 (defn update-todo-done-field!
-  "updates todo adn done lists. Also updates metadata like file lengths"
+  "updates todo and done lists in state atom.
+  Also updates file lengths metadata"
   [state-atom kw thing]
   (swap! state-atom assoc kw thing)
   (swap! state-atom assoc :todo-files-count (count (:todo-files @state-atom)))
   (swap! state-atom assoc :done-files-count (count (:done-files @state-atom))))
+
+
+(defn file-list
+  []
+  (seesaw/border-panel :north
+                       (seesaw/scrollable (seesaw/listbox :model ["f1" "f2" "f3"]))
+                       :south (seesaw/label "metadata here")))
+
+(def next_image_gui
+  (seesaw/frame :title "next image v0.1-dev"
+                                  :size [400 :by 600]
+                                  :content (file-list)))
+
+(defn start-app
+  [gui]
+  (-> gui
+      seesaw/show!))
+
+(defn mod [content]
+  (seesaw/config! next_image_gui
+                  :content content)
+  content)
+
+(start-app next_image_gui)
+
+#_(mod (seesaw/label "new stuff"))
+
--- test/next_image/core_test.clj
+++ test/next_image/core_test.clj
@@ -4,12 +4,13 @@
             [next-image.core :as core]))
 
 (deftest update-atom
-  (let [todos ["f1" "f2" "f3"]
-        dones ["d1" "d2"]]
-    (core/update-todo-done-field! core/next-image-state :todo-files todos)
-    (is (= todos (:todo-files @core/next-image-state)))
-    (is (= (count todos) (:todo-files-count @core/next-image-state)))
-    (core/update-todo-done-field! core/next-image-state :done-files dones)
-    (is (= dones (:done-files @core/next-image-state)))
-    (is (= (count dones) (:done-files-count @core/next-image-state)))))
+  (testing "basic state update functions"
+    (let [todos ["f1" "f2" "f3"]
+          dones ["d1" "d2"]]
+      (core/update-todo-done-field! core/next-image-state :todo-files todos)
+      (is (= todos (:todo-files @core/next-image-state)))
+      (is (= (count todos) (:todo-files-count @core/next-image-state)))
+      (core/update-todo-done-field! core/next-image-state :done-files dones)
+      (is (= dones (:done-files @core/next-image-state)))
+      (is (= (count dones) (:done-files-count @core/next-image-state))))))
 
