473670513c6a1781adb88a198dc6e61db161a052
Nicholas George <nicholas.george32@gmail.com>
2020-03-16 08:40:18 -0600

    add fn to handle input. if nil display message.

--- src/next_image/core.clj
+++ src/next_image/core.clj
@@ -2,9 +2,12 @@
   (:require [seesaw.core :as seesaw]))
 
 ;; left off on line 182 of https://gist.github.com/daveray/1441520 where it starts to get useful
+;; https://imagej.net/Tips_for_developers#Tips_for_Graphical_User_Interface_.28GUI.29_programming
 ;; see listbox example https://github.com/daveray/seesaw/blob/master/test/seesaw/test/examples/scroll.clj
+;; seesaw api http://daveray.github.io/seesaw/seesaw.core-api.html
+;; https://github.com/daveray/seesaw/blob/master/test/seesaw/test/examples/temp.clj
+
 (seesaw/native!)
-;; list files
 
 (def next-image-state
   "holds all state for app"
@@ -16,22 +19,32 @@
 (defn update-todo-done-field!
   "updates todo and done lists in state atom.
   Also updates file lengths metadata"
-  [state-atom kw thing]
-  (swap! state-atom assoc kw thing)
+  [state-atom k thing]
+  (swap! state-atom assoc k thing)
   (swap! state-atom assoc :todo-files-count (count (:todo-files @state-atom)))
   (swap! state-atom assoc :done-files-count (count (:done-files @state-atom))))
+
+
+(defn update-field!
+  [state-atom k alt-msg]
+  (if (= nil (k @state-atom))
+    [alt-msg]
+         (k @state-atom)))
 
 
 (defn file-list
   []
   (seesaw/border-panel :north
-                       (seesaw/scrollable (seesaw/listbox :model ["f1" "f2" "f3"]))
+                       (seesaw/scrollable
+                        (seesaw/listbox :model (update-field! next-image-state :todo-files "No images found...")))
                        :south (seesaw/label "metadata here")))
 
 (def next_image_gui
   (seesaw/frame :title "next image v0.1-dev"
-                                  :size [400 :by 600]
-                                  :content (file-list)))
+                :width 400
+                :height 600
+                :content (file-list)
+                :id "main"))
 
 (defn start-app
   [gui]
@@ -43,7 +56,10 @@
                   :content content)
   content)
 
-(start-app next_image_gui)
+#_(start-app next_image_gui)
 
 #_(mod (seesaw/label "new stuff"))
+#_(seesaw/dispose! (seesaw/all-frames))
+
+#_(update-todo-done-field! next-image-state :todo-files ["test1" "test2"])
 
