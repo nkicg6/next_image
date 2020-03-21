47bbd8294fbf73d755c4ff3f087eba2127af8f99
Nicholas George <nicholas.george32@gmail.com>
2020-03-21 10:31:13 -0600

    display files and update ui based on dir choice

--- src/next_image/gui.clj
+++ src/next_image/gui.clj
@@ -1,13 +1,24 @@
 (ns next-image.gui
   (:require [seesaw.core :as seesaw]
             [seesaw.chooser :as chooser]
-            [seesaw.font :as font]))
+            [seesaw.font :as font]
+            [next-image.file-utils :refer [get-files!]]))
 
 (def app-state (atom ""))
 
-(defn get-files [e]
+(defn update-listbox
+  [frame fend]
+  (seesaw/config! frame :content
+                  (seesaw/border-panel :north (seesaw/scrollable
+                                               (seesaw/listbox :model (get-files! @app-state fend)))
+                                       :south (seesaw/left-right-split (seesaw/label :text "metadata here") (seesaw/button :text "choose a different project" :listen [:action #(chooser-dialogue % base-frame ".txt")]))
+                                       :hgap 5 :vgap 10 :border 10)))
+
+(defn chooser-dialogue [_ frame fend]
   (->> (chooser/choose-file :dir "~" :type :open :selection-mode :dirs-only)
-       (reset! app-state)))
+       (reset! app-state))
+  (update-listbox frame fend))
+
 
 (def base-frame
   (seesaw/frame
@@ -15,15 +26,20 @@
    :title "next_image v0.1-dev"
    :width 400 :height 400
    :content
-   (seesaw/border-panel :center
-                        (seesaw/label :text "<html>Welcome to <i>next image</i>!<br></br>Select the folder containing the images you are analyzing below to get started"
-                :font "ARIAL-21"
-                :h-text-position :center)
-                        :south
-                        (seesaw/button :text "Select your image folder"
-                 :id :select-dirs
-                 :listen [:action #(get-files %)])
-                        :hgap 5 :vgap 10 :border 10)))
+   (seesaw/border-panel
+    :center
+    (seesaw/label :text "<html>Welcome to <i>next image</i>!<br></br>Select the folder containing the images you are analyzing below to get started"
+                  :font "ARIAL-21"
+                  :h-text-position :center)
+    :south
+    (seesaw/button :text "Select your image folder"
+                   :id :select-dirs
+                   :listen [:action #(chooser-dialogue % base-frame ".txt")])
+    :hgap 5 :vgap 10 :border 10)))
+
+;; to update and quickly check the layout:
+#_(update-listbox base-frame ".txt")
+
 
 (-> base-frame seesaw/show!)
 
@@ -31,4 +47,5 @@
   (seesaw/dispose! base-frame))
 
 @app-state
+
 
